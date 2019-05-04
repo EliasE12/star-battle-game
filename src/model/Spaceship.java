8b2b2257932974a6f7902623aaf14f90a8f332a1
Oscar Riascos <osvan202@gmail.com>
2019-05-04 13:50:00 -0500

    Creado el constructor y los metodos get y set de los atrbitos de la clase spaceShip

--- src/model/Spaceship.java
+++ src/model/Spaceship.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Spaceship {
+public abstract class Spaceship {
 
 	//Relaciones
 	private Spaceship next;
@@ -32,6 +32,103 @@ public class Spaceship {
 	 * Salud de la nave.
 	 */
 	protected int healt;
+	
+	
+	public Spaceship(String name, int staffLimit, String model, double volume, double fuelRatio, int healt) {
+		this.name = name;
+		this.staffLimit = staffLimit;
+		this.model = model;
+		this.volume = volume;
+		this.fuelRatio = fuelRatio;
+		this.healt = healt;
+		this.next = null;
+		this.previus = null;
+	}
+
+
+	public Spaceship getNext() {
+		return next;
+	}
+
+
+	public void setNext(Spaceship next) {
+		this.next = next;
+	}
+
+
+	public Spaceship getPrevius() {
+		return previus;
+	}
+
+
+	public void setPrevius(Spaceship previus) {
+		this.previus = previus;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getStaffLimit() {
+		return staffLimit;
+	}
+
+
+	public void setStaffLimit(int staffLimit) {
+		this.staffLimit = staffLimit;
+	}
+
+
+	public String getModel() {
+		return model;
+	}
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+	public double getVolume() {
+		return volume;
+	}
+
+
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+
+
+	public double getFuelRatio() {
+		return fuelRatio;
+	}
+
+
+	public void setFuelRatio(double fuelRatio) {
+		this.fuelRatio = fuelRatio;
+	}
+
+
+	public int getHealt() {
+		return healt;
+	}
+
+
+	public void setHealt(int healt) {
+		this.healt = healt;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
