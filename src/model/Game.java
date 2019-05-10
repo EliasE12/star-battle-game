eb9a286b6452b16612172c7d98b8e6645cf4da5e
Oscar Riascos <osvan202@gmail.com>
2019-05-10 18:11:06 -0500

    Se creó el metodo recursivo searchPlayer en la clase Player y en la clase Game, del mismo modo se creo el metodo que verifica si un nodo es una hoja o no

--- src/model/Game.java
+++ src/model/Game.java
@@ -10,23 +10,28 @@ public class Game {
     public Game() {
         this.root = null;
     }
-
-    public Player searchPlayer(String nickName) throws EmptyDataException {
-        Player found = null;
-
+    /**
+     * -------
+     *  Se usa recursiva para localizar un jugador en el arbol que inicia en este nodo.
+     * @param nickName es el nombre de usuario que se desea buscar. - nameSearch != null
+     * @return jugador con el mismo nombre de usuario. Si no lo encuentra retorna null;
+     * @throws NullPointerException se lanzá cuando no encuentra a un jugador.
+     * @throws EmptyDataException se lanzá cuando no se ingresa un valor vacio.
+     */
+    public Player searchPlayer(String nickName) throws EmptyDataException, NullPointerException{
+    	Player found = null;
         if (nickName.equals("")){
             throw new EmptyDataException();
-
         }else {
-
-
-            if (found == null){
-                throw new NullPointerException();
-            }
+        	found = (root == null) ? null: root.search(nickName);
+        }
+        if(found == null) {
+        	throw new NullPointerException();
         }
         return found;
     }
 
+  
     public void addPlayer(String nickName, String name) throws EqualUserException {
         Player player = new Player(name ,nickName, 0, 0, 0);
 
@@ -36,4 +41,6 @@ public class Game {
             root.addPlayer(player);
         }
     }
+    
+    	
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -68,4 +68,33 @@ public class Player {
             }
         }
     }
+    
+    /**
+     * -------
+     *  Se usa recursiva para localizar un jugador en el arbol que inicia en este nodo.
+     * @param nickName es el nombre de usuario que se desea buscar. - nameSearch != null
+     * @return jugador con el mismo nombre de usuario. Si no lo encuentra retorna null;
+     * @throws NullPointerException se lanzá cuando no encuentra a un jugador.
+     */
+    public Player search (String nickName) throws NullPointerException{
+    	
+    	if(this.name.compareTo(nickName) == 0) {
+    		return this;
+    	}else if(this.name.compareTo(nickName) < 0) {
+    		return (right == null)? null : right.search(nickName);    		
+      	}else {
+      		return (left == null)? null : left.search(nickName);
+    	}
+    }
+    
+    
+    /**
+     * Verifica si el nodo actual de este arbol es un hoja.
+     * Es hoja si no tiene nodos hijos.
+     * @return Verdadero si es una hoja, si es lo contrario falso.
+     */
+    public boolean isLeaf() {
+    	return (right == null && left == null);
+    }
+    
 }
