5592fb3e490f85b24d41793990fd3c1ec8912b9b
EliasE12 <simply753@gmail.com>
2019-05-10 07:26:02 -0500

    Implementadas la ventana de Hall of Fame, con su respectivo controller

--- src/controllers/FallOfFameController.java
+++ src/controllers/FallOfFameController.java
@@ -1,16 +1,30 @@
 package controllers;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.Player;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FallOfFameController implements Initializable {
 
+    @FXML private TableView<Player> tvHallOfFame;
+    @FXML private TableColumn<Player, String> tcName;
+    @FXML private TableColumn<Player, Integer> tcGlobalScore;
+    @FXML private TableColumn<Player, Integer> tcWonMatch;
+    @FXML private TableColumn<Player, Integer> tcLostMatch;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
     }
+
+
 
 
 
