6f8ddd2ff8aaf2330fbf8c5b0bc00f15bb9dd14b
gomezLF <gomez_pipe99@hotmail.com>
2019-05-24 16:48:06 -0500

    Creado el metodo que inicializa la creacion de las naves al momento de comenzar la partida. Creada los metodos que verifican las casillas del tablero de juego, para ver si hay o no naves alli ubicadas.

--- src/model/Faction.java
+++ src/model/Faction.java
@@ -1,9 +1,9 @@
 package model;
+import model.Match.Direction;
 
 public class Faction {
 
     public enum SpaceShipType{STARFIGHTER, BOMBER, INTERCEPTOR, GUNSHIP, DREADNOUGHT, DESTROYER, BATTLECRUISER, SHUTTLE};
-    public enum Direction{VERTICAL,HORIZONTAL}
 
     private Node root;
     private Spaceship first;
@@ -20,7 +20,7 @@ public class Faction {
     }
 
 
-    public boolean verificarPosition(String[][] gb,int posF, int postC, Direction direction, int sizeSpaceshit) {
+    private boolean verificarPosition(String[][] gb, int posF, int postC, Direction direction, int sizeSpaceshit) {
         boolean valido = false;
         if (direction.equals(Direction.HORIZONTAL)) {
             int size = 0;
@@ -55,16 +55,22 @@ public class Faction {
                 }
             }
         }
-
         return valido;
     }
 
 
+    /**
+     * Crea posiciones en X y Y aleatorias, para verificar si es posible posicionar la nave ha crear en dicha posicion.
+     * <pre> la matriz en la cual se va ha posicionar la nave tiene que ser != null</>
+     * @param type el tipo de nave ha crear
+     * @param size el tamano de la nave
+     * @param gameBoard la matriz en la cual se posicionara la nave
+     */
     public void createRandomNumbers(SpaceShipType type, int size, String[][] gameBoard) {
-        Match.DIRECTION direction;
+        Direction direction = null;
         int randomX = 0;
         int randomY = 0;
-        int randomD = 0;
+        int randomD;
 
         boolean posicion = false;
 
@@ -73,39 +79,45 @@ public class Faction {
             randomY = (int) Math.floor(Math.random() * (0 - (Match.GAME_BOARD_SIZE - 1)) + ((Match.GAME_BOARD_SIZE - 1) - 1));
             randomD = (int) Math.floor(Math.random() * (1 - (2)) + (2 - 1));
 
-        }
+            if (randomD == 1){
+                direction = Direction.HORIZONTAL;
+            }else {
+                direction = Direction.VERTICAL;
+            }
 
-        if (randomD == 1){
-            direction = Match.DIRECTION.HORIZONTAL;
-        }else {
-            direction = Match.DIRECTION.VERTICAL;
-        }
+            posicion = verificarPosition(gameBoard, randomX, randomY, direction, size);
 
+        }
         posicionarNave(gameBoard, randomX, randomY, direction, size);
     }
 
-    public void posicionarNave(String[][] matriz,int x, int y, Match.DIRECTION direction, int  size_Nave){
+
+    public void posicionarNave(String[][] matriz, int x, int y, Direction direction, int  size_Nave){
         boolean termino = false;
 
-        switch (direction) {
-            case HORIZONTAL:
-                for (int j = y; !termino; j++) {
-                    matriz[x][j] = "X";
-                    size_Nave--;
-                    if (size_Nave < 1) {
-                        termino = true;
+        if (verificarPosition(matriz, x, y, direction, size_Nave)){
+
+            switch (direction) {
+                case HORIZONTAL:
+                    for (int j = y; !termino; j++) {
+                        matriz[x][j] = "X";
+                        size_Nave--;
+                        if (size_Nave < 1) {
+                            termino = true;
+                        }
                     }
-                }
-                break;
-            case VERTICAL:
-                for (int i = x; !termino; i++) {
-                    matriz[i][y] = "X";
-                    size_Nave--;
-                    if (size_Nave < 1) {
-                        termino = true;
+                    break;
+
+                case VERTICAL:
+                    for (int i = x; !termino; i++) {
+                        matriz[i][y] = "X";
+                        size_Nave--;
+                        if (size_Nave < 1) {
+                            termino = true;
+                        }
                     }
-                }
-                break;
+                    break;
+            }
         }
     }
 
--- src/model/Match.java
+++ src/model/Match.java
@@ -1,13 +1,18 @@
 package model;
 
 import customExceptions.EmptyDataException;
+import model.Faction.SpaceShipType;
 
 public class Match {
 
+    public static final long DURATION_MATCH = 600000;
+    public static final long DURATION_TURN = 60000;
+
     public static final int GAME_BOARD_SIZE = 10;
 
+
     public enum LeaderType {DOMINATOR, PROTECTOR, STRATEGIST};
-    public enum DIRECTION{HORIZONTAL, VERTICAL};
+    public enum Direction {HORIZONTAL, VERTICAL};
 
 
     private Faction user;
@@ -66,11 +71,109 @@ public class Match {
         }
     }
 
-    public void createPlayerShips(){
+    /**
+     * Se encarga de calcular el tamano de la nave pasada por parametro.
+     * @param spaceShipType tipo de nave a calcular el tamano
+     * @return El tamano de la nave especificada por parametro
+     */
+    private int calculateSizeShip(SpaceShipType spaceShipType){
+        int size = 0;
 
+        switch (spaceShipType){
+            case BOMBER:
+                size = Bomber.SIZE;
+                break;
+
+            case GUNSHIP:
+                size = Gunship.SIZE;
+                break;
+
+            case SHUTTLE:
+                size = Shuttle.SIZE;
+                break;
+
+            case DESTROYER:
+                size = Destroyer.SIZE;
+                break;
+
+            case DREADNOUGHT:
+                size = Dreadnought.SIZE;
+                break;
+
+            case INTERCEPTOR:
+                size = Interceptor.SIZE;
+                break;
+
+            case STARFIGHTER:
+                size = Starfigther.SIZE;
+                break;
+
+            case BATTLECRUISER:
+                size = Battlecruiser.SIZE;
+                break;
+        }
+        return size;
     }
 
-    public void createMachineShips(){
 
+    /**
+     * Inicializa el llamado a los metodos que se encargan de crear las naves en las posiciones escogidas por el jugador.
+     * <pre>La matriz gameBoardPlayer donde se van a posicionar las naves del jugador, debe ser != null</>
+     * <pre>La matriz gameBoardMachine donde se van a posicionar las naves de la maquina, debe ser != null</>
+     * @param spaceShipType tipo de nave a crear y/o posicionar
+     * @param direction direccion en la que se va a posicionar la nave
+     * @param x posicion X de la nave
+     * @param y posicion Y de la nave
+     */
+    public void createSpaceShips(SpaceShipType spaceShipType, Direction direction, int x, int y){
+        int size;
+        size = calculateSizeShip(spaceShipType);
+
+        user.posicionarNave(gameBoardPlayer, x, y, direction, size);
+        machine.createRandomNumbers(spaceShipType, size, gameBoardMachine);
+    }
+
+
+    /**
+     * Se encarga de verificar que si en la posicion, pasada como parametro, haya ubicada una nave en el tablero de juego del jugador.
+     * @param x posicion X de la nave
+     * @param y posicion Y de la nave
+     * @return true si en la posicion, pasada por parametro, hay una nave. False en caso contrario.
+     */
+    public boolean uncoverPlayersBox(int x, int y){
+        boolean discoveredShip = false;
+
+        if (gameBoardPlayer[x][y].equals("X")){
+            discoveredShip = true;
+        }
+
+        return discoveredShip;
+    }
+
+
+    /**
+     * Se encarga de verificar que si en la posicion, pasada como parametro, haya ubicada una nave en el tablero de juego de la maquina.
+     * @param x posicion X de la nave.
+     * @param y posicion Y de la nave.
+     * @return True si en la posicion pasada como parametro hay una nave. False en caso contrario.
+     */
+    public boolean uncoverMachineBox(int x, int y){
+        boolean discoveredShip = false;
+
+        if (gameBoardMachine.equals("X")){
+            discoveredShip = true;
+        }
+
+        return discoveredShip;
+    }
+
+
+
+    public boolean winner(){
+        boolean win = false;
+
+
+
+        return win;
     }
 }
