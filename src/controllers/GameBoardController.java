35c8f59c27a1e3b7cdbab23152a4f11e6033d62d
EliasE12 <simply753@gmail.com>
2019-05-24 22:15:56 -0500

    Creada la ventana del tablero de juego e implementado el controlador de la ventana.

---
+++ src/controllers/GameBoardController.java
@@ -0,0 +1,80 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameBoardController implements Initializable {
+
+    @FXML private Label gameTimeMachine;
+    @FXML private Label gameTimePlayer;
+    @FXML private Label lbTimeGlobal;
+    @FXML private JFXButton btDreadNought;
+    @FXML private JFXButton btBattleCruiser;
+    @FXML private VBox destroyerClicked;
+    @FXML private JFXButton btDestroyer;
+    @FXML private JFXButton btStarFighter;
+    @FXML private JFXButton btBomber;
+    @FXML private JFXButton btInterceptor;
+    @FXML private JFXButton btGunShip;
+    @FXML private JFXButton btShuttle;
+    @FXML private JFXComboBox<?> cbDirection;
+    @FXML private JFXButton playClicked;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+    @FXML public void battleCruiserClicked(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    public void bomberClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void destroyerClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void dreadNoughtClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void gunShipClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void interceptorClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void shuttleClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void starFighterClicked(ActionEvent event) {
+
+    }
+
+
+}
